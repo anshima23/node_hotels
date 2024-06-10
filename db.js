b51a973582530390e8ec1159cf1fb3c579bee410
anshima23 <141510818+anshima23@users.noreply.github.com>
2024-06-10 19:47:52 +0530

    adding databse

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURL = process.env.MONGODB_URL;
+const mongoURL = process.env.DB_URL;
 
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
